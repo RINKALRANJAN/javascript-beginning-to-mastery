6739d2f861523a7c552ec4eed84cb98729b87f4d
RINKALRAJAN <rinkalranjan2468@gmail.com>
2023-05-28 20:18:12 +0530

    some changes

--- part1/28.js
+++ part1/28.js
@@ -11,11 +11,26 @@
 
 // reference types 
 // array 
-let array1 = ["item1", "item2"];
-let array2 = array1;
-console.log("array1", array1);
-console.log("array2", array2);
-array1.push("item3");
-console.log("after pushing element to array 1");
-console.log("array1", array1);
-console.log("array2", array2);
+// let array1 = ["item1", "item2"];
+// let array2 = array1;
+// console.log("array1", array1);
+// console.log("array2", array2);
+// array1.push("item3");
+// console.log("after pushing element to array 1");
+// console.log("array1", array1);
+// console.log("array2", array2);
+let num1 = 6;
+num1++;
+let num2 = num1;
+
+console.log(num1);
+console.log(num2);
+let arr1 = ["item1", "item2"];
+let array2 = arr1;
+console.log(arr1);
+console.log(array2);
+arr1.push("hey ther");
+console.log(arr1);
+console.log(array2);
+// array refernce type h
+// var,let primitive type ...ye stack m store hota h
