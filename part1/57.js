fb952da94d200b6a8b90d3f3c66658ef6f139e31
RINKALRAJAN <rinkalranjan2468@gmail.com>
2023-06-01 20:57:14 +0530

    new

--- part1/57.js
+++ part1/57.js
@@ -1,23 +1,23 @@
 // map method 
-// const numbers = [3,4,6,1,8];
+const numbers = [3, 4, 6, 1, 8];
 
-// const square = function(number){
-//     return number*number;
-// }
+const square = function(number) {
+    return number * number;
+}
 
-// const squareNumber = numbers.map((number, index)=>{
+// const squar eNumber = numbers.map((number, index)=>{
 //     return index;
 // });
 // console.log(squareNumber);
 
 const users = [
-    {firstName: "harshit", age: 23},
-    {firstName: "mohit", age: 21},
-    {firstName: "nitish", age: 22},
-    {firstName: "garima", age: 20},
+    { firstName: "harshit", age: 23 },
+    { firstName: "mohit", age: 21 },
+    { firstName: "nitish", age: 22 },
+    { firstName: "garima", age: 20 },
 ]
 
-const userNames = users.map((user)=>{
+const userNames = users.map((user) => {
     return user.firstName;
 });
 
--- part1/66.js
+++ part1/66.js
@@ -8,6 +8,7 @@
 // }
 
 const items = ['item1', 'item2', 'item3'];
+
 // for(let item of items){
 //     console.log(item);
 // }
